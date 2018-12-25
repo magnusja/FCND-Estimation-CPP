9f540db7be80681641514bd11b2688215bc7492f
Magnus <jahnen@in.tum.de>
2018-12-25 19:45:15 +0100

    complementary filter (scenario 7)

--- src/QuadEstimatorEKF.cpp
+++ src/QuadEstimatorEKF.cpp
@@ -93,9 +93,17 @@ void QuadEstimatorEKF::UpdateFromIMU(V3F accel, V3F gyro)
   // (replace the code below)
   // make sure you comment it out when you add your own code -- otherwise e.g. you might integrate yaw twice
 
-  float predictedPitch = pitchEst + dtIMU * gyro.y;
-  float predictedRoll = rollEst + dtIMU * gyro.x;
-  ekfState(6) = ekfState(6) + dtIMU * gyro.z;	// yaw
+  auto q = Quaternion<float>::FromEuler123_RPY(rollEst, pitchEst, ekfState(6));
+
+  auto integratedQ = q.IntegrateBodyRate(gyro, dtIMU);
+
+  float predictedPitch = integratedQ.Pitch();
+  float predictedRoll = integratedQ.Roll();
+  ekfState(6) = integratedQ.Yaw();
+
+  //float predictedPitch = pitchEst + dtIMU * gyro.y;
+  //float predictedRoll = rollEst + dtIMU * gyro.x;
+  //ekfState(6) = ekfState(6) + dtIMU * gyro.z;	// yaw
 
   // normalize yaw to -pi .. pi
   if (ekfState(6) > F_PI) ekfState(6) -= 2.f*F_PI;
